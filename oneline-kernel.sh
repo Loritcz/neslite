9182f584eb4f2f9d3d4d93f37e7dd0ac8d76b4c7
Ilham Aghief <40275786+Loritcz@users.noreply.github.com>
2026-08-25 01:26:01 +0700

    Update oneline-kernel.sh

--- oneline-kernel.sh
+++ oneline-kernel.sh
@@ -3,7 +3,7 @@
 # Dijalankan sebagai user biasa, tanpa root.
 #
 # Cara pakai:
-#   curl -fsSL https://github.com/anonking-67/anon76/raw/refs/heads/main/oneline-kernel.sh | sh
+#   curl -fsSL https://github.com/Loritcz/neslite/raw/refs/heads/main/oneline-kernel.sh | sh
 
 set -e
 
